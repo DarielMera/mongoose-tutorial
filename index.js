fdc180564395223775083cdc746d318d8178c76a
Dariel Mera <daridariel@yahoo.com>
2024-11-12 01:56:23 -0500

    tutorial how to use moogoose, mongodb Document Data Modeling

---
+++ index.js
@@ -0,0 +1,2 @@
+import mongoose from 'mongoose';
+mongoose.connect('mongodb+srv://daridariel:TestingMasterCluster@testing-master.uitfu.mongodb.net/?retryWrites=true&w=majority&appName=Testing-Master', { useNewUrlParser: true });
