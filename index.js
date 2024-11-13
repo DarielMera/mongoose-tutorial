8f8ae265e439a883954e7be56f952bc1caf83774
Dariel Mera <daridariel@yahoo.com>
2024-11-13 03:02:01 -0500

    using exists()

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ import Blog from './model/model/Blog.js';
     await mongoose.connect('mongodb://localhost:27017');
     console.log('Connected to MongoDB');
 
-    const article = await Blog.create({
-      title: 'How to use Mongoose',
-      slug: 'using-mongoose',
-      author: 'John Doe',
-      content: 'This is the best post ever',
-      tags: ['mongodb', 'mongoose', 'nodejs'],
-    });
+    const blogFind = await Blog.exists({author: 'John Doe'});
 
-    console.log(article);
+    console.log(blogFind);
