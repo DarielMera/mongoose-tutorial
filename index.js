273302facedf03c5c06047d4ad5da11d1b7da8c5
Dariel Mera <daridariel@yahoo.com>
2024-11-13 03:29:49 -0500

    using multiple schemas

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import mongoose from 'mongoose';
 import Blog from './model/model/Blog.js';
+import User from './model/model/User.js';
 
     await mongoose.connect('mongodb://localhost:27017');
     console.log('Connected to MongoDB');
 
-    const blogFind = await Blog.where('author').equals('John Doe').select('title author')
+    const user = await User.create({
+        name: 'John Doe',
+        email: 'jhoneDoeNew@nose.com',
+    });
 
-    console.log(blogFind);
+    const article = await Blog.create({
+        title: 'New Article',
+        slug: 'new-article',
+        author: user._id,
+        content: 'This is a new article',
+        tags: ['new', 'article'],
+    });
+
+    console.log(article);
--- model/model/Blog.js
+++ model/model/Blog.js
@@ -1,38 +1,45 @@
 import mongoose from "mongoose";
-const { Schema, model} = mongoose;
+const { Schema, model } = mongoose;
 
-const blogSchema = new Schema({ 
-    title: {
-        type: String,
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: String,
+  tags: [String],
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+  updatedAt: Date,
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
         required: true,
-    },  
-    slug: {
-        type: String,
-        required: true,
-        lowrcase: true
+      },
+      content: String,
+      votes: Number,
     },
-    published: {
-        type: Boolean, 
-        default: false
-    },
-    author: {
-        type: String,
-        required: true,
-    },
-    content: String,
-    tags: [String],
-    createdAt: {
-        type: Date,
-        default: ()=> Date.now(),
-        inmutable: true,
-    },
-    updatedAt: Date,
-    Comments: [{
-        user: String,
-         content: String,
-         votes: Number,
-    }]
-}); 
+  ],
+});
 
-const Blog = model('Blog', blogSchema);
+const Blog = model("Blog", blogSchema);
 export default Blog;
