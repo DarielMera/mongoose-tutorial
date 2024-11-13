8dfea98315d0c39106fbbc440cb91abedb5a3a93
Dariel Mera <daridariel@yahoo.com>
2024-11-13 03:04:23 -0500

    using where()

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import Blog from './model/model/Blog.js';
     await mongoose.connect('mongodb://localhost:27017');
     console.log('Connected to MongoDB');
 
-    const blogFind = await Blog.exists({author: 'John Doe'});
+    const blogFind = await Blog.where('author').equals('John Doe')
 
     console.log(blogFind);
