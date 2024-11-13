bcfd1ae56021eda6a46200c2a513187d6a063b7f
Dariel Mera <daridariel@yahoo.com>
2024-11-13 02:18:52 -0500

    using findByIdmethod and the exec() function

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 import mongoose from 'mongoose';
-mongoose.connect('mongodb+srv://daridariel:TestingMasterCluster@testing-master.uitfu.mongodb.net/?retryWrites=true&w=majority&appName=Testing-Master', { useNewUrlParser: true });
+import Blog from './model/model/Blog.js';
+
+    await mongoose.connect('mongodb://localhost:27017');
+    console.log('Connected to MongoDB');
+
+    // Create a new blog post object
+    // const article = await Blog.create({
+    //   title: 'How to use Mongoose',
+    //   slug: 'using-mongoose',
+    //   published: true,
+    //   content: 'This is how you use Mongoose to interact with MongoDB',
+    //   tags: ['mongodb', 'mongoose', 'nodejs'],
+    //   createdAt: new Date().toLocaleString(),
+    // });
+
+    // article.title = 'How to use Mongoose with Node.js';
+    // await article.save();
+
+const article = await Blog.findById('6734512165afdcf256b97265').exec();
+
+    console.log(article);
