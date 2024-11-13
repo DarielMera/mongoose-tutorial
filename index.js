d5281e3707ed71fe320c8e7a25671f6cd0c81d32
Dariel Mera <daridariel@yahoo.com>
2024-11-13 02:56:16 -0500

    using objects to model the values of the fields to be created

--- index.js
+++ index.js
@@ -4,19 +4,12 @@ import Blog from './model/model/Blog.js';
     await mongoose.connect('mongodb://localhost:27017');
     console.log('Connected to MongoDB');
 
-    // Create a new blog post object
-    // const article = await Blog.create({
-    //   title: 'How to use Mongoose',
-    //   slug: 'using-mongoose',
-    //   published: true,
-    //   content: 'This is how you use Mongoose to interact with MongoDB',
-    //   tags: ['mongodb', 'mongoose', 'nodejs'],
-    //   createdAt: new Date().toLocaleString(),
-    // });
+    const article = await Blog.create({
+      title: 'How to use Mongoose',
+      slug: 'using-mongoose',
+      author: 'John Doe',
+      content: 'This is the best post ever',
+      tags: ['mongodb', 'mongoose', 'nodejs'],
+    });
 
-    // article.title = 'How to use Mongoose with Node.js';
-    // await article.save();
-
-const article = await Blog.deleteMany
-({ title: 'How to use Mongoose' });
     console.log(article);
--- model/model/Blog.js
+++ model/model/Blog.js
@@ -2,13 +2,30 @@ import mongoose from "mongoose";
 const { Schema, model} = mongoose;
 
 const blogSchema = new Schema({ 
-    title: String,  
-    slug: String,
-    published: Boolean,
-    author: String,
+    title: {
+        type: String,
+        required: true,
+    },  
+    slug: {
+        type: String,
+        required: true,
+        lowrcase: true
+    },
+    published: {
+        type: Boolean, 
+        default: false
+    },
+    author: {
+        type: String,
+        required: true,
+    },
     content: String,
     tags: [String],
-    createdAt: Date,
+    createdAt: {
+        type: Date,
+        default: ()=> Date.now(),
+        inmutable: true,
+    },
     updatedAt: Date,
     Comments: [{
         user: String,
